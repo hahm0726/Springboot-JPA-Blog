5305f6f583664444d30463133d505375587cc9e7
Hahm <hahm0726@naver.com>
2021-10-05 16:48:38 +0900

    환경세팅완료

---
+++ src/main/java/com/blog/hahmlog/HahmlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.hahmlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahmlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HahmlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/hahmlog/HahmlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.hahmlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahmlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
