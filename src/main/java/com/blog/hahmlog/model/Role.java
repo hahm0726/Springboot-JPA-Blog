917b72f9b9e40a3fdfb23d5f36b146a864e5dd0f
Hahm <hahm0726@naver.com>
2021-10-12 01:01:36 +0900

    enum 사용 이유 주석 작성

--- src/main/java/com/blog/hahmlog/model/Role.java
+++ src/main/java/com/blog/hahmlog/model/Role.java
@@ -1,5 +1,9 @@
 package com.blog.hahmlog.model;
 
+
 public enum Role {
+    // enum의 장점
+    // 도메인(어떤 데이터의 범위)를 제공해줌으로써 데이터의 오류를 줄일 수 있음
+    // 도메인을 지정해 제공해 줌으로 써 사용자의 입력으로부터 오는 데이터의 오류를 줄일 수 있다
     ADMIN,MANAGER,USER
 }
